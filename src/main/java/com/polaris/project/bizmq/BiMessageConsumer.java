82e6b1cc5a1c825d0d4e430b9489e50e8e404e30
Polarisronx <polarisronx@163.com>
2024-06-26 17:02:43 +0800

    [✨|3]增加图片可保存功能

--- src/main/java/com/polaris/project/bizmq/BiMessageConsumer.java
+++ src/main/java/com/polaris/project/bizmq/BiMessageConsumer.java
@@ -1,12 +1,19 @@
 package com.polaris.project.bizmq;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import com.polaris.project.model.entity.Chart;
+import com.polaris.project.model.entity.User;
+import com.polaris.project.service.UserService;
 import com.rabbitmq.client.Channel;
 import com.polaris.project.common.ErrorCode;
 import com.polaris.project.constant.CommonConstant;
 import com.polaris.project.exception.BusinessException;
 import com.polaris.project.manager.AiManager;
 import com.polaris.project.service.ChartService;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -14,8 +21,13 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
 import javax.annotation.Resource;
+import java.io.IOException;
+
+import static com.polaris.project.utils.MyWebSocketHandler.sendMessage;
+import static com.polaris.project.utils.MyWebSocketHandler.webSocketMap;
 
 @Component
 @Slf4j
@@ -25,13 +37,16 @@ public class BiMessageConsumer {
     private ChartService chartService;
 
     @Resource
+    private UserService userService;
+    @Resource
     private AiManager aiManager;
 
     // 指定程序监听的消息队列和确认机制
     @SneakyThrows
     @RabbitListener(queues = {BiMqConstant.BI_QUEUE_NAME}, ackMode = "MANUAL")
-    public void receiveMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) {
+    public void receiveMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag){
         log.info("receiveMessage message = {}", message);
+
         if (StringUtils.isBlank(message)) {
             // 如果失败，消息拒绝
             channel.basicNack(deliveryTag, false, false);
@@ -43,6 +58,9 @@ public class BiMessageConsumer {
             channel.basicNack(deliveryTag, false, false);
             throw new BusinessException(ErrorCode.NOT_FOUND_ERROR, "图表为空");
         }
+
+        Long userId = chart.getUserId();
+        String chartName = chart.getName();
         // 先修改图表任务状态为 “执行中”。等执行成功后，修改为 “已完成”、保存执行结果；执行失败后，状态修改为 “失败”，记录任务失败信息。
         Chart updateChart = new Chart();
         updateChart.setId(chart.getId());
@@ -53,6 +71,7 @@ public class BiMessageConsumer {
             handleChartUpdateError(chart.getId(), "更新图表执行中状态失败");
             return;
         }
+        System.out.println("--------------------------------");
         // 调用 AI
         String result = aiManager.doChat(CommonConstant.BI_MODEL_ID, buildUserInput(chart));
         String[] splits = result.split("【【【【【");
@@ -63,17 +82,40 @@ public class BiMessageConsumer {
         }
         String genChart = splits[1].trim();
         String genResult = splits[2].trim();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode=null;
+        try {
+            // 解析为JsonNode
+            jsonNode = objectMapper.readTree(genChart);
+            // 检查是否已有"toolbox"键
+            if (!jsonNode.has("toolbox")) {
+                // 如果没有，添加"toolbox"
+                JsonNode toolboxNode = objectMapper.createObjectNode();
+                ((ObjectNode) toolboxNode).putObject("feature").putObject("saveAsImage");
+                ((ObjectNode) jsonNode).set("toolbox", toolboxNode);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String updatedGenChart = objectMapper.writeValueAsString(jsonNode);
+
+
+
         Chart updateChartResult = new Chart();
         updateChartResult.setId(chart.getId());
-        updateChartResult.setGenChart(genChart);
+        updateChartResult.setGenChart(updatedGenChart);
         updateChartResult.setGenResult(genResult);
         // todo 建议定义状态为枚举值
         updateChartResult.setStatus("succeed");
         boolean updateResult = chartService.updateById(updateChartResult);
         if (!updateResult) {
+
             channel.basicNack(deliveryTag, false, false);
             handleChartUpdateError(chart.getId(), "更新图表成功状态失败");
         }
+        String successMsg = "您的[" + chartName + "]生成成功 , 前往 我的图表 进行查看";
+        sendMessage(String.valueOf(1790712036790730753L), successMsg);
         // 消息确认
         channel.basicAck(deliveryTag, false);
     }
